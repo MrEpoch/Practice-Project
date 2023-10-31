f3f66761fb5796a969db414e55982c944e05addd
MrEpoch <alexandrstencuk@seznam.cz>
2023-10-31 13:29:19 +0100

    Some layout of pricing page is built

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
 	import { currentPage } from 'lib/stores';
 	import { onMount } from 'svelte';
+  import { products } from "./pricing";
 
 	onMount(() => {
 		$currentPage = 'pricing';
-	});
+  });
 </script>
 
-<div class="min-h-screen w-full dark:bg-darkmode-500" />
+<div class="min-h-screen w-full dark:bg-darkmode-500">
+  <div class="max-w-screen-xl py-16 px-4 mx-auto w-full">
+    <div class="grid grid-cols-1 gap-x-6 gap-y-10 w-full
+        sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+    {#each products as product}
+      <a href={`/services/${product.id}`} class="items-center group relative flex flex-col max-w-sm p-6 bg-white border-purple-600 border-[2px] rounded-xl shadow dark:bg-darkmode-400 hover:brightness-90 dark:hover:brightness-75 transition duration-300 ">
+        <h5 class="mb-2 text-xl text-center font-bold tracking-tight text-gray-900 dark:text-white">{product.name}</h5>
+        <div class="overflow-hidden rounded-lg"><img class="rounded-lg group-hover:scale-110 duration-300 overflow-hidden transition" src={product.image} alt={product.name} /></div>
+        <div class="p-6" />
+        <p class="mb-3 text-gray-700 text-2xl absolute bottom-2 right-2 dark:text-gray-400 font-bold">${product.price}</p>
+      </a>
+    {/each}
+        </div>
+  </div>
+</div>
---
+++ src/routes/pricing/pricing.ts
@@ -0,0 +1,28 @@
+import BasicWarrior from "assets/BasicWarrior.jpg";
+
+export const products = [
+  {
+    id: 1,
+    name: "Basic Warrior VR",
+    price: 100,
+    image: BasicWarrior
+  },
+  {
+    id: 1,
+    name: "Advanced Warrior VR",
+    price: 100,
+    image: BasicWarrior
+  },
+  {
+    id: 1,
+    name: "Skilled Warrior VR",
+    price: 100,
+    image: BasicWarrior
+  },  
+  {
+    id: 1,
+    name: "Master Warrior VR",
+    price: 100,
+    image: BasicWarrior
+  },
+]
